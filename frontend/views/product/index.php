6fe1f6a78d4981121d086b414fed2fbe0e113998
SlavKoVrn <slavko.chita@gmail.com>
2023-07-19 20:16:00 +0300

    product pagination

--- frontend/views/product/index.php
+++ frontend/views/product/index.php
@@ -2,6 +2,7 @@
 /** @var yii\web\View $this */
 \common\assets\DataTableAsset::register($this);
 ?>
+<ul class="pagination"></ul>
 <table id="myTable" class="display">
     <thead>
         <tr>
@@ -12,27 +13,66 @@
         </tr>
     </thead>
 </table>
+<ul class="pagination"></ul>
+<style>
+    ul.pagination > li {
+        padding:12px;
+    }
+    ul.pagination > li.active {
+        color:white;
+        background-color:lightgrey;
+    }
+</style>
 <?php
 $js=<<<JS
-    $.ajax({
-        type:'get',
-        url: '/api/product',
-        data:{
-            'page':2
-        },
-        success: function (data) {
-            $('#myTable').DataTable({
-                data:data,
-                columns: [
-                    { data: 'id' },
-                    { data: 'category.name' },
-                    { data: 'name' },
-                    { data: 'description' },
-                ]
-            });
-        },
-        error: function () {
-        },
+    $(document).on('click','ul.pagination li',function(e){
+        e.preventDefault();
+        window.pagination($(this).data('page'));
     });
+    window.pagination = function(page){
+        $.ajax({
+            type:'get',
+            url: '/api/product',
+            data:{
+                'page':page
+            },
+            success: function(data, status, jqXHR) {
+                var current_page = jqXHR.getResponseHeader('x-pagination-current-page');
+                var page_count   = jqXHR.getResponseHeader('x-pagination-page-count');
+                var per_page     = jqXHR.getResponseHeader('x-pagination-per-page');
+                var total_count  = jqXHR.getResponseHeader('x-pagination-total-count');
+                var paginationHtml = '';
+                for (var i = 1; i <= page_count; i++) {
+                    if (i === Number(current_page)) {
+                        paginationHtml += '<li class="active" data-page="' + i + '">';
+                    } else {
+                        paginationHtml += '<li data-page="' + i + '">';
+                    }
+                    paginationHtml += '<a href="/api/product?page=' + i + '">' + i + '</a></li>';
+                }
+                $('.pagination').html(paginationHtml);
+                if ( $.fn.dataTable.isDataTable( '#myTable' ) ) {
+                    $('#myTable').DataTable().clear().draw();
+                    $('#myTable').DataTable().rows.add(data).draw();
+                }else{
+                    $('#myTable').DataTable({
+                        data:data,
+                        'pageLength': per_page,
+                        'paging':false,
+                        'searching':false,
+                        columns: [
+                            { data: 'id' },
+                            { data: 'category.name' },
+                            { data: 'name' },
+                            { data: 'description' },
+                        ]
+                    });
+                }
+            },
+            error: function () {
+            },
+        });
+    }
+    window.pagination(1);
 JS;
 $this->registerJS($js);
