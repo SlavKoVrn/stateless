ec9bbd66b03fb7554db59cecd62283d42fb55a32
SlavKoVrn <slavko.chita@gmail.com>
2023-07-23 00:56:40 +0300

    api tests

---
+++ frontend/tests/_data/api.user.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    [
+        'username' => 'admin',
+        'auth_key' => 'b5ecfb677419669ff34c7060d9c147d4',
+        'password_hash' => '$2y$13$VJm6ejYONRBxi/YBaT5zR.Q5/4X68/8ycWbxSCUjsvRoWio1Iix62',
+        'password_reset_token' => 't5GU9NwpuGYSfb7FEZMAxqtuz2PkEvv_' . time(),
+        'created_at' => '1689654368',
+        'updated_at' => '1689960416',
+        'email' => 'slavko.chita@gmail.com',
+        'status' => '10',
+        'token' => 'f8fc7d0bdfe9ec82fc0d9acb147b9738',
+        'expired_at' => '1690046816',
+    ],
+    [
+        'username' => 'troy.becker',
+        'auth_key' => 'EdKfXrx88weFMV0vIxuTMWKgfK2tS3Lp',
+        'password_hash' => '$2y$13$g5nv41Px7VBqhS3hVsVN2.MKfgT3jFdkXEsMC4rQJLfaMa7VaJqL2',
+        'password_reset_token' => '4BSNyiZNAuxjs5Mty990c47sVrgllIi_' . time(),
+        'created_at' => '1391885313',
+        'updated_at' => '1391885313',
+        'email' => 'nicolas.dianna@hotmail.com',
+        'status' => '0',
+        'token' => 'EdKfXrx88weFMV0vIxuTMWKgfK2tS3Lp',
+        'expired_at' => '1690046816',
+    ],
+];
---
+++ frontend/tests/api/ApiCest.php
@@ -0,0 +1,54 @@
+<?php
+namespace frontend\tests\functional;
+
+use common\fixtures\UserFixture;
+use frontend\tests\FunctionalTester;
+
+class ApiCest
+{
+    public function _fixtures()
+    {
+        return [
+            'user' => [
+                'class' => UserFixture::class,
+                'dataFile' => codecept_data_dir() . 'api.user.php',
+            ],
+        ];
+    }
+
+    public function badMethod(FunctionalTester $I)
+    {
+        $I->expectTo('badMethod');
+        $I->sendGET('/auth');
+        $I->seeResponseCodeIs(404);
+    }
+
+    public function wrongCredentials(FunctionalTester $I)
+    {
+        $I->sendPOST('/auth',[
+            'username' => 'erau',
+            'password' => 'wrong-password'
+        ]);
+        $I->seeResponseCodeIs(422);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'field' => 'password',
+            'message' => 'Incorrect username or password.',
+        ]);
+    }
+
+    public function success(FunctionalTester $I)
+    {
+        $I->expectTo('correct auth');
+        $I->sendPOST('/auth',[
+            'username' => 'admin',
+            'password' => '123456'
+        ]);
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->canSeeResponseJsonMatchesJsonPath('$.token');
+        $I->canSeeResponseJsonMatchesJsonPath('$.expired');
+    }
+
+
+}
