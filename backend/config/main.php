4ed528437ff7799bd9dbc11742868d7f58612db9
SlavKoVrn <slavko.chita@gmail.com>
2025-01-29 13:04:31 +0300

    ProGorod Kirov

--- backend/config/main.php
+++ backend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-backend',
+    'name' => 'ProГород Киров',
     'language' => 'ru-RU',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
